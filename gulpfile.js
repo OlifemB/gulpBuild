d43ec7ef91b359112fe622cf6fe5ca83a2e46a78
Olifem JSX <32907240+OlifemB@users.noreply.github.com>
2022-05-30 19:56:22 +0300

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 const gulp = require("gulp");
 const del = require("del");
 const sass = require("gulp-sass")(require("sass"));
-const rename = require("gulp-rename");
 const cleanCSS = require("gulp-clean-css");
 const babel = require("gulp-babel");
 const uglify = require("gulp-uglify");
 const concat = require("gulp-concat");
+const sourcemaps  = require("gulp-sourcemaps");
+const autoprefixer = require("gulp-autoprefixer");
 
 const paths = {
   styles: {
@@ -23,22 +24,29 @@ const clean = () => {
 };
 
 const styles = () => {
-  return gulp
-    .src(paths.styles.src)
+  return gulp.src(paths.styles.src)
+    .pipe(sourcemaps.init())
     .pipe(sass())
-    .pipe(cleanCSS())
+    .pipe(autoprefixer({
+        cascade: false
+    }))
+    .pipe(cleanCSS({
+        level: 2
+    }))
     .pipe(concat("main.min.css"))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(paths.styles.dest));
 };
 
 const scripts = () => {
-  return gulp
-    .src(paths.scripts.src, {
-      sourcemap: true,
-    })
-    .pipe(babel())
+  return gulp.src(paths.scripts.src)
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
     .pipe(uglify())
     .pipe(concat("main.min.js"))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(paths.scripts.dest));
 };
 
